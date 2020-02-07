194ae6865298944ead8329a4ac526478ba8220bf
Gagan Daroach <daroachgb@msoe.edu>
2020-02-07 01:58:15 -0600

    update deploy training script

--- run_training_rosie.sh
+++ run_training_rosie.sh
@@ -2,8 +2,8 @@
 # MSOE ROSIE run StyleGAN2
 # author: gagany <daroachgb@msoe.edu>
 
-set MCW_RESEARCH=/srv/data/mcw_research
-set TRAINING_DATA=$MCW_RESEARCH/tfrecord/0.5x
-set OUTPUT=$MCW_RESEARCH/stylegan2/training_output
-
-python run_training.py --num-gpus=8 --data-dir=$OUTPUT --config=config-f --dataset=$TRAINING_DATA --mirror-augment=false
+MCW_RESEARCH=/srv/data/mcw_research
+TRAINING_DATA=0.5x_cleaned
+DATA_DIR=$MCW_RESEARCH/tiles/tfrecords
+echo $TRAINING_DATA
+python run_training.py --num-gpus=8 --data-dir=$DATA_DIR --config=config-f --dataset=$TRAINING_DATA --mirror-augment=true
