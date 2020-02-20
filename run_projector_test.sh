3ae78760550c32cd8dca01671a2bbc6c0d582b38
Gagan Daroach <daroachgb@msoe.edu>
2020-02-20 15:16:56 -0600

    update with MSOE ROSIE data path

--- run_projector_test.sh
+++ run_projector_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-network_path=/Users/gagandaroach/code/network-snapshot-016015.pkl
+network_path=/srv/data/mcw_research/stylegan2/networks/network-snapshot-016015.pkl
 
 python run_projector.py project-generated-images --network=$network_path --seeds=0,1,5
